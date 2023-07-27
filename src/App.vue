fff1ff5e305d28a0ba92019955c8f4185868f349
JiseopSong <46867559+Bigdata92@users.noreply.github.com>
2023-07-28 00:00:11 +0900

    03 Vue Component
    vue 구성요소
    1. template(html)
    2. script(js)
    3. style(css)
    
    script에서 변수 생성 하여 이를 template에서 사용시,
    1. export default{} 선언
    2. setup() 선언
    3. 변수 선언 및 할당
    4. 해당 변수 return
    5. template에서 {{ 변수명 }} 으로 사용

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,20 @@
-<template>
-  <div>test</div>
+<template>  <!-- vue 구성요소1 template - html -->
+  <div class="name-style">{{ name }}</div> <!-- script 요소 사용시 {{ 변수명 }} -->
 </template>
+
+<script>  // vue 구성요소2 script - js
+export default {  // 1. export default {} 선언
+  setup() {       // 2. setup() 선언
+    const name = 'test1'; // 3. 변수 선언 및 할당
+    return {      // 4. 변수 return
+      name,
+    };
+  }
+}
+</script>
+
+<style> /* vue 구성요소3 style - css */
+.name-style {
+  color: red;
+}
+</style>
