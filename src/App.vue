98101362f5b7c7b2a3ada1e15b6421471a3e5499
JiseopSong <wltkqdl@gmail.com>
2023-07-29 12:36:01 +0900

    10 리스트에 새로운 To-Do 추가
    1. list 생성 : const todos = ref([]);
    2. list object 삽입 :
    todos.value.push({  // []에 object push
        id: Date.now(),  // unique
        subject: todo.value
    });
    3. form tag 제출
    <form action="" @submit.prevent="onSubmit">

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>  <!-- vue 구성요소1 template - html -->
-  <input type="text" :value="name"> <!-- 단방향, 화면(template) 에서 값 변경시, 변수(script) 에서 값 변경 x -->
-  <input type="text" v-model="age"> <!-- 양방향, 화면(template) 에서 값 변경시, 변수(script) 에서 값 변경 o -->
-  <button class="btn btn-primary" @click="onSubmit">
-    Click
-  </button>
+  <div class="container"> <!-- bootstrap class : 왼쪽, 오른쪽 띄어줌-->
+    <h2>To-Do List</h2>
+    <form action="" class="d-flex" @submit.prevent="onSubmit"> <!-- @submit : 제출시 함수 사용 / .prevent(Modifier) : 새로고침x  -->
+      <div class="flex-grow-1 mr-2"> <!-- 너비 늘어나게  -->
+        <input type="text" v-model="todo" class="form-control" placeholder="Type new to-do"> <!-- 너비에 text 맞추기  -->
+      </div>
+      <div>
+        <button class="btn btn-primary" type="submit">
+          Click
+        </button>
+      </div>
+    </form>
+  </div>
+  {{ todos }}
 </template>
 
 <script>  // vue 구성요소2 script - js
@@ -11,19 +20,22 @@ import {ref} from 'vue';
 
 export default {  
   setup() {       
-    const name = ref('test1');
-    const age = ref(10);
+    const todo = ref('');
+    const todos = ref([]);
 
     const onSubmit = () => {
-      console.log(name.value);
-      console.log(age.value);
+      console.log(todo.value);
+      todos.value.push({  // []에 object push
+        id: Date.now(),  // unique
+        subject: todo.value
+      });
     }
 
 
     return {      
-      name,
+      todo,
+      todos,
       onSubmit,
-      age,
 
     };
   }
