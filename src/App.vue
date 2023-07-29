19dde50b404a7c2c247e5c6d54731230efbebb9b
JiseopSong <wltkqdl@gmail.com>
2023-07-29 20:26:50 +0900

    17 make form component
    src/components/x.vue 이름으로 만들고
    이를 import 해와서 <x/> 로 사용
    form component로 만들 시 이전처럼 작동 x -> 다음 강의

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,7 @@
 <template>  <!-- vue 구성요소1 template - html -->
   <div class="container"> <!-- bootstrap class : 왼쪽, 오른쪽 띄어줌-->
     <h2>To-Do List</h2>
-    <form action="" @submit.prevent="onSubmit"> <!-- @submit : 제출시 함수 사용 / .prevent(Modifier) : 새로고침x  -->
-      <div class="d-flex">
-        <div class="flex-grow-1 mr-2"> <!-- 너비 늘어나게  -->
-          <input type="text" v-model="todo" class="form-control" placeholder="Type new to-do"> <!-- 너비에 text 맞추기  -->
-        </div>
-        <div>
-          <button class="btn btn-primary" type="submit">
-            Click
-          </button>
-        </div>
-      </div>
-      <div v-show="hasError" style="color: red;">
-        This field cannot be empty
-      </div>
-    </form>
+    <TodoSimpleForm/>
     <div v-if="!todos.length">  <!-- todos 원소 개수가 0일때, 보여줌 -->
       추가된 todo가 없습니다.
     </div>
@@ -36,8 +22,12 @@
 
 <script>  // vue 구성요소2 script - js
 import {ref} from 'vue';
+import TodoSimpleForm from '@/components/TodoSimpleForm.vue'
 
 export default {  
+  components: {
+    TodoSimpleForm,
+  },
   setup() {       
     const todo = ref('');
     const todos = ref([]);
---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,25 @@
+<template lang="">
+    <form action="" @submit.prevent="onSubmit"> <!-- @submit : 제출시 함수 사용 / .prevent(Modifier) : 새로고침x  -->
+      <div class="d-flex">
+        <div class="flex-grow-1 mr-2"> <!-- 너비 늘어나게  -->
+          <input type="text" v-model="todo" class="form-control" placeholder="Type new to-do"> <!-- 너비에 text 맞추기  -->
+        </div>
+        <div>
+          <button class="btn btn-primary" type="submit">
+            Click
+          </button>
+        </div>
+      </div>
+      <div v-show="hasError" style="color: red;">
+        This field cannot be empty
+      </div>
+    </form>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
